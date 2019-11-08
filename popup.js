2dede70825d822e7dd4cb60a470cc0020193a55d
lycoris11 <markpinto@Marks-MacBook-Pro.local>
2019-11-08 17:46:05 -0500

    whoops

--- popup.js
+++ popup.js
@@ -1,15 +1,17 @@
-document.getElementById("runBetToggle").addEventListener('change', function(){
-	if(this.checked){
-		startBetting();
-	}else{
-		stopBetting();
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById("runBetToggle").addEventListener('change', function(){
+		if(this.checked){
+			startBetting();
+		}else{
+			stopBetting();
+		}
+	});
+
+	function startBetting() {
+		alert("Betting Started!");
+	}
+
+	function stopBetting() {
+		alert("Betting Stopped!");
 	}
 });
-
-function startBetting(){
-	alert("Betting Started!");
-}
-
-function stopBetting(){
-	alert("Betting Stopped!");
-}
