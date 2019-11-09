a3cec5a0ac34d688182e093eb3e95fe5ef305785
lycoris11 <markpinto@Marks-MacBook-Pro.local>
2019-11-09 02:26:05 -0500

    change to button

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById("runBetToggle").addEventListener('change', function(){
-		if(this.checked){
+	document.getElementById("runBetToggle").addEventListener('click', function(){
+		if(this.clicked){
 			alert("Betting Started!");
 			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             	chrome.tabs.executeScript(tabs[0].id, {file: "page.js"});
