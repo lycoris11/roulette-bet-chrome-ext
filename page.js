2b87c0ffaa2dec995e526f1f66cb5778d1e90a80
fang570 <georgecfang99@gmail.com>
2019-11-10 03:13:28 -0500

    Adding changes

--- page.js
+++ page.js
@@ -1,7 +1,4 @@
-var run = true;
-
-
-startRunning(run);
+var startMsg = "I'm Dumb";
 
 function clickPenny() {
     document.getElementsByClassName("bet-input__control")[1].click();
@@ -15,10 +12,54 @@ function clickGold(){
 	document.getElementsByClassName("bet-btn")[2].click()
 }
 
-function toggleRun(){
-	if(run){
-		run = false;
-	}else{
-		run = true;
-	}
+function getBalance(){
+  return document.getElementsByClassName("balance")[1].getElementsByTagName("span")[0].getElementsByTagName("span")[0].innerText;
+}
+
+function getBlackCount(){
+  return document.getElementsByClassName("text-light-grey-1 text-xxs font-bold mr-2")[3].innerText;
+}
+
+function getGoldCount(){
+  return document.getElementsByClassName("text-light-grey-1 text-xxs font-bold mr-2")[5].innerText;
+}
+
+
+chrome.runtime.onMessage.addListener(
+      function(request, sender, sendResponse) {
+        window.startMsg = request.msg;
+        if(request.msg === "start" ) {
+          beginBetting();
+        }
+});
+
+
+
+
+
+
+
+
+
+function beginBetting(){
+  var stopOrRun = true;
+    var balance = getBalance();
+    clickPenny();
+
+    var click = false;
+    var checkTime = setInterval(function(){
+      if($())
+    })
+      if(getBlackCount > getGoldCount){
+        clickBlack();
+      }else if(getBlackCount === getGoldCount){
+        clickBlack();
+      }else{
+        clickGold();
+      }
+    });
+
+    if(tempBalance>balance)
+
+  }
 }
--- popup.js
+++ popup.js
@@ -1,19 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById("runBetToggle").addEventListener('click', function(){
-		if(this.clicked){
-			alert("Betting Started!");
-			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            	chrome.tabs.executeScript(tabs[0].id, {file: "page.js"});
-        	});
-		}
-	});
-
-	document.getElementById("stopBet").addEventListener('click', function(){
-		if(this.clicked){
-			alert("Betting Started!");
-			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            	chrome.tabs.executeScript(tabs[0].id, {file: "page.js"}, clearInterval());
-        	});
-		}
-	});
+	document.getElementById("runBetToggle").addEventListener('click', popup);
 });
+
+
+function popup() {
+	 chrome.runtime.sendMessage({msg: "start"});
+}
