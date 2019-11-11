5ea6d538b38566eb1bb3334361fb7c503d6d03e1
lycoris11 <markpinto@Marks-MacBook-Pro.local>
2019-11-11 04:09:35 -0500

    Comments and bugfixes

--- page.js
+++ page.js
@@ -1,48 +1,54 @@
+//sets a penny bet
 function clickPenny() {
-    document.getElementsByClassName("bet-input__control")[1].click();
+  document.getElementsByClassName("bet-input__control")[1].click();
 }
 
+//sets a dime bet
 function clickDime() {
-    document.getElementsByClassName("bet-input__control")[2].click();
+  document.getElementsByClassName("bet-input__control")[2].click();
 }
 
+//sets 1 dollar bet
 function clickDollar() {
-    document.getElementsByClassName("bet-input__control")[3].click();
+  document.getElementsByClassName("bet-input__control")[3].click();
 }
 
+//doubles bet
+function clickDouble(){
+  document.getElementsByClassName("bet-input__control")[7].click();
+}
+
+//clicks the black bet
 function clickBlackBet(){
 	document.getElementsByClassName("bet-btn")[0].click()
 }
 
+//clicks the gold bet
 function clickGoldBet(){
 	document.getElementsByClassName("bet-btn")[2].click()
 }
 
+//returns balance
 function getBalance(){
   return document.getElementsByClassName("balance")[1].getElementsByTagName("span")[0].getElementsByTagName("span")[0].innerText;
 }
 
+//returns number of past black wins
 function getBlackCount(){
   return document.getElementsByClassName("text-light-grey-1 text-xxs font-bold mr-2")[3].innerText;
 }
 
+//returns number of past gold bet wins
 function getGoldCount(){
   return document.getElementsByClassName("text-light-grey-1 text-xxs font-bold mr-2")[5].innerText;
 }
 
+//clears the bet
 function clickClear(){
   document.getElementsByClassName("ml-3 button-pill uppercase")[0].click();
 }
 
-
-chrome.runtime.onMessage.addListener(
-      function(request, sender, sendResponse) {
-        window.startMsg = request.msg;
-        if(request.msg === "start" ) {
-          beginBetting();
-        }
-});
-
+//clicks on the bet that has the most previous wins
 function clickBlackOrGold(){
   if(getBlackCount()>=getGoldCount()){
     clickBlackBet();
@@ -55,23 +61,39 @@ function clickBlackOrGold(){
 
 function beginBetting(){
   console.log("Inside Begin Betting!");
-  var balance = getBalance();
+  var initialBalance = getBalance();
+  //Click on a Dime
   clickDime();
-  
+  //Bet
+  clickBlackOrGold();
+  //Check balance afterwards
   afterBetBal = getBalance();
   setInterval(function(){
+    //Get the balance after the bet
     afterBetBal = getBalance();
-    if(balance >= afterBetBal){
+    //If the balance after bet is less than initial
+    //you lose and double your bet
+    if(afterBetBal <= initialBalance){
       console.log("Double Bet!");
-      document.getElementsByClassName("bet-input__control")[7].click();
-      balance = getBalance();
+      clickDouble();
+      initialBalance = getBalance();
       clickBlackOrGold();
     }else{
+      //If balance after bet is greater than inital
+      //you win, clear bet, click dime, and bet
       console.log("We Won!");
       clickClear();
       clickDime();
-      balance = getBalance();
+      initialBalance = getBalance();
       clickBlackOrGold();
     }
-  },28000);
+  },27500);
 }
+
+chrome.runtime.onMessage.addListener(
+      function(request, sender, sendResponse) {
+        window.startMsg = request.msg;
+        if(request.msg === "start" ) {
+          beginBetting();
+        }
+});
