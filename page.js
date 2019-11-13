f652f193db754c2fe060d1fc6ee3ddb4b996989d
lycoris11 <pinto9435@gmail.com>
2019-11-13 00:34:11 -0500

    Better Betting with Last Win Class Checking

--- page.js
+++ page.js
@@ -28,6 +28,16 @@ function clickGoldBet(){
 	document.getElementsByClassName("bet-btn")[2].click()
 }
 
+//clears the bet
+function clickClear(){
+  document.getElementsByClassName("ml-3 button-pill uppercase")[0].click();
+}
+
+//input custom bet
+function clickTextInput(customBetVal){
+  document.getElementsByClassName("bg-transparent w-full h-full")[0].value = customBetVal;
+}
+
 //returns balance
 function getBalance(){
   return document.getElementsByClassName("balance")[1].getElementsByTagName("span")[0].getElementsByTagName("span")[0].innerText;
@@ -43,14 +53,8 @@ function getGoldCount(){
   return document.getElementsByClassName("text-light-grey-1 text-xxs font-bold mr-2")[5].innerText;
 }
 
-//clears the bet
-function clickClear(){
-  document.getElementsByClassName("ml-3 button-pill uppercase")[0].click();
-}
-
-//input custom bet
-function clickTextInput(customBetVal){
-  document.getElementsByClassName("bg-transparent w-full h-full")[0].value = customBetVal;
+function getWinningBetClass(){
+  return document.getElementsByClassName("previous-rolls-item")[19].firstElementChild.className;
 }
 
 //clicks on the bet that has the most previous wins
@@ -58,9 +62,11 @@ function clickBlackOrGold(){
   if(getBlackCount()>=getGoldCount()){
     clickBlackBet();
     console.log("Betting Black!");
+    return "inline-block w-24 h-24 rounded-full ml-1 coin-ct";
   }else{
     clickGoldBet();
     console.log("Betting Gold!");
+    return "inline-block w-24 h-24 rounded-full ml-1 coin-t";
   }
 }
 
@@ -69,16 +75,17 @@ function beginBetting(){
   var initialBalance = getBalance();
   //Click on a Dime
   clickDime();
+  clickDime();
   //Bet
   clickBlackOrGold();
   //Check balance afterwards
-  afterBetBal = getBalance();
+  var afterBetBal = getBalance();
   setInterval(function(){
     //Get the balance after the bet
     afterBetBal = getBalance();
     //If the balance after bet is less than initial
     //you lose and double your bet
-    if(afterBetBal <= initialBalance){
+    if(afterBetBal < initialBalance){
       console.log("Double Bet!");
       clickDouble();
       initialBalance = getBalance();
@@ -89,10 +96,34 @@ function beginBetting(){
       console.log("We Won!");
       clickClear();
       clickDime();
+      clickDime();
       initialBalance = getBalance();
       clickBlackOrGold();
     }
   },28000);
+}
+
+function betterBeginBetting(){
+  console.log("Inside Better Betting");
+  //click bet ammount
+  clickDime();
+  clickDime();
+  //return the class of the coin we bet on
+  var ourBetClass = clickBlackOrGold();
+  setInterval(function(){
+    //if the coin matches the winning class then we double down
+    if(ourBetClass == getWinningBetClass()){
+      console.log("We Won!");
+      clickClear();
+      clickDime();
+      clickDime();
+      ourBetClass = clickBlackOrGold();
+    }else{
+      console.log("Double Bet!");
+      clickDouble();
+      ourBetClass = clickBlackOrGold();
+    }
+  }, 28000);
 }
 
 function beginCustomBetting(customBetVal){
@@ -130,7 +161,7 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     window.startMsg = request.msg;
     if(request.msg === "start" && request.customBetVal === "") {
-      beginBetting();
+      betterBeginBetting();
     }else if(request.msg === "start" && request.customBetVal.length > 0){
       beginCustomBetting(request.customBetVal);
     }
